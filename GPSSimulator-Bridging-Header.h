a597d640913c340c13555103fad02ea3cfae3c42
Jaromir Siska <siskaj@me.com>
2015-02-02 18:43:58 +0100

    Pridani GPX framework do projektu

---
+++ GPSSimulator-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+#import <UIKit/UIkit.h>
+#endif
+
+
+#import <GPX/GPX.h>
