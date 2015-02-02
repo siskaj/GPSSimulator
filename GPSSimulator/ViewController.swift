e9d1575e55fc8c89b8a654d94b5467f90f1887c0
Jaromir Siska <siskaj@me.com>
2015-02-02 08:46:59 +0100

    Initial Commit

---
+++ GPSSimulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSSimulator
+//
+//  Created by Jaromir Siska on 02.02.15.
+//  Copyright (c) 2015 Baltoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
